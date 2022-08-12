ae887e9a51eef6c4462b2b78f44d225f80a22799
Eliana García <elianagarcia862@gmail.com>
2022-08-12 10:19:22 -0300

    cambios

--- js/products.js
+++ js/products.js
@@ -1,19 +1,47 @@
-let url="https://japceibal.github.io/emercado-api/cats_products/101.json"
-fetch(url)
-.then(response => response.json())
-.then(data => mostrarData(data))
-.catch(error => console.log(error))
+//Se referencia el origen de los datos en formato Json
+const url="https://japceibal.github.io/emercado-api/cats_products/101.json"
 
-const mostrarData= (data)=> {
+//array donde se cargarán los datos recibidos
+let productsArray = [];
+//función que realiza el fetch() a la url recibida y devuelve un objeto con los datos
+
+function showProductsList(data) {
     console.log(data)
-    let body="";
-    for(let i=0; i<data.length; i++){
-        let category= data[i];
+    let body=""; //se crea un let vacio
+    console.log(data["products"].length)
+    data["products"].forEach(products => { //recorre el array
+       console.log(products)
         body+=`
-        <div class="row"> 
+            <div class="row">
+                <div class="col-3">
+                    <img src="${products.image}" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>${products.name + " - " + products.currency + products["cost"]}</h4> 
+                        <p> ${products.description}</p> 
+                        </div>
+                        <small class="text-muted"> ${products.soldCount}  vendidos</small> 
+                    </div>
+
+                </div>
+            </div>
         </div>
         `
+       
+    });
+    
+    document.getElementById("data").innerHTML = body;
+    console.log(body)
     }
-    document.getElementById("data").innerHTML = body
-    }
+
  
+    document.addEventListener("DOMContentLoaded", function(e){
+        fetch(url) // se realiza una solicitud a esta url
+        .then(response => response.json())// se resuelve la promesa y al obtener respuesta la pasa a formato json
+        .then(data => {
+            return showProductsList(data);
+        })
+        .catch(error => console.log(error))// si hay error lo atrapa y lo muestra por consola
+    });
