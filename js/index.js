5da6fb3bcb06198bde59e2677f524ade50a50a50
Eliana García <elianagarcia862@gmail.com>
2022-08-15 15:05:56 -0300

    entrega 1

--- js/index.js
+++ js/index.js
@@ -1,14 +1,30 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html"
-    });
-});
+var form = document.getElementById("loginForm");
+
+form.addEventListener("submit", validateForm, true);
+
+function validateForm(event) {
+    event.preventDefault()
+   let valorEmail= document.forms ["myForm"]["email"].value;
+   let valorContraseña= document.forms ["myForm"]["contraseña"].value;
+   if (valorEmail ==""){
+       let inputEmail=document.querySelector("#emailInput")
+       inputEmail.style.border="1px solid red"
+       let indicacionEm = document.getElementById("indicacionEm")
+       indicacionEm.innerHTML= "Ingresa tu Email"
+       indicacionEm.style.color="red"
+       indicacionEm.style.fontSize="13px"
+      }
+      if (valorContraseña ==""){
+        let inputPass=document.querySelector("#passInput")
+        inputPass.style.border="1px solid red"
+
+        let indicacionPass=document.getElementById("indicacionPass")
+        indicacionPass.innerHTML="Ingresa tu contraseña" 
+        indicacionPass.style.color="red"
+        indicacionPass.style.fontSize="13px"
+      }
+      if(valorEmail !=="" && valorContraseña !==""){
+        window.location.href="home.html"
+      }
+}
+
